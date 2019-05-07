a0376870a06088d5391df7779f5a80748819c455
Kit Jonathan Te <kjonathante@gmail.com>
2019-05-07 04:49:13 -0700

    Did yarn build on client. Added app.get('/*') on server.

---
+++ client/build/precache-manifest.440e71dd11ef64661869f3e5340272c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00bd24c0d598f77234635c46223f6c06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "d7462877e38a494a1b4b",
+    "url": "/static/js/main.9275243e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- server/index.js
+++ server/index.js
@@ -23,6 +23,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 }
 app.use(routes);
 
